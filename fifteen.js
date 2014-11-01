68abaf018ed0ff4388d311b2907464de5310514b
alexm52 <alexmcdoom@hotmail.com>
2014-11-01 21:55:03 +0000

    Implemented findEmpty() to locate the empty square's posn for after shuffle

--- fifteen.js
+++ fifteen.js
@@ -1,17 +1,55 @@
+/*VAR DECLARATION*/
+var tiles;
+var btn_shuffle;
+var x_offset;
+var y_offset;
+var blank_x;
+var blank_y;
+
+/*ONLOAD - SETS EVERYTHING UP*/
 window.onload = function(){
-    var tiles = $$("#puzzlearea div");
-    var col_offset = 0;
-    var row_offset = 0;
+    tiles = $$("#puzzlearea div");
+    x_offset = 0;
+    y_offset = 0;
     for(var i=0; i<tiles.length; i++){
         tiles[i].addClassName("puzzlepiece");
         tiles[i].setAttribute('id', 'piece'+(i+1));
-        if(col_offset===4){
-            col_offset=0;
-            row_offset++;
+        if(x_offset===4){
+            x_offset=0;
+            y_offset++;
         }
-        tiles[i].style.left = col_offset*100 + "px";
-        tiles[i].style.top = row_offset*100 + "px";
-        tiles[i].style.backgroundPosition = col_offset*-100 + "px " + row_offset*-100 + "px";
-        col_offset++;
+        tiles[i].style.left = x_offset*100 + "px";
+        tiles[i].style.top = y_offset*100 + "px";
+        tiles[i].style.backgroundPosition = x_offset*-100 + "px " + y_offset*-100 + "px";
+        x_offset++;
     }
+    
+    btn_shuffle = $$("#shufflebutton")[0];
+    //console.log("logloglog");
+    btn_shuffle.onclick = function(){
+        //shuffle code here;
+        console.log("button click");
+        findEmpty();
+        
+    }
+}
+
+/*ADDITIONAL FUNCTION DEFINITIONS*/
+
+/*findEmpty() - sets blank_x and blank_y to x and y coordinates 
+of the missing square based on the positions of the 'puzzlepiece' 
+elements*/
+var findEmpty = function(){
+    console.log("find empty");
+    var x_cnt = 0;
+    var y_cnt = 0;
+    for(var i=0; i<tiles.length; i++){
+        //console.log("position: " + tiles[i].style.left + ", " + tiles[i].style.top);
+        x_cnt += ((parseInt(tiles[i].style.left)/100)+1);
+        y_cnt += ((parseInt(tiles[i].style.top)/100)+1);
+    }
+    //console.log(x_cnt + " " + y_cnt);
+    blank_x = ((40-x_cnt)-1)*100;
+    blank_y = ((40-y_cnt)-1)*100;
+    //console.log("blank_x: " + blank_x + ", blank_y: " + blank_y);
 }
